9149b5a643d7638a5e74d8f8bf663bcdef014b3c
kobitoko <kobitoko@gmail.com>
2016-10-10 01:42:25 -0600

    markdown bool in DB, unsafely just accepts and executes html spit out by users tho.

--- mysite/socknet/models.py
+++ mysite/socknet/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+import CommonMark
 
 class Post(models.Model):
     """ Represents a post made by a user """
     author = models.ForeignKey(User)
     content = models.TextField(max_length=512)
     created_on = models.DateTimeField(auto_now=True)
+    markdown = models.BooleanField()
 
     def get_absolute_url(self):
         """ Gets the canonical URL for a Post
@@ -15,8 +17,11 @@ class Post(models.Model):
         return reverse('view_post', args=[str(self.id)])
 
     def view_content(self):
-        #return self.content.replace("\n","<br>")
-        return self.content.split("\n")
+        if(self.markdown):
+            return CommonMark.commonmark(self.content)
+        return self.content.replace("\n", "<br/>")
         
-    def __str__(self):
+    # enable weird characters like lenny faces taken from:
+    #http://stackoverflow.com/questions/36389723/why-is-django-using-ascii-instead-of-utf-8
+    def __unicode__(self):
         return self.author.username + ": " + self.content
