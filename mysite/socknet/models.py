83991f62eb6e7633e7466a5c1e35bfe55cef08a8
Victoria Bobey <vbobey@ualberta.ca>
2016-10-19 11:08:56 -0600

    Changed Author model to contain who i'm following instead of my followers to make accept friend request and delete friend request only alter us

--- mysite/socknet/models.py
+++ mysite/socknet/models.py
@@ -20,58 +20,59 @@ class Author(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
     # Friends and followers are separate --> I can be both a friend and a follower
-    # Ignore is for friend requests you have declined, just means it won't show up as pending
-    friends = models.ManyToManyField("self", related_name="friend_of", blank=True)
-    followers = models.ManyToManyField("self", related_name="follower_of", symmetrical=False, blank=True)
-    ignore = models.ManyToManyField("self", related_name="ignored_by", symmetrical=False, blank=True)
+    # ignored is for friend requests you have declined, just means it won't show up as pending
+    friends = models.ManyToManyField("self", related_name="my_friends", blank=True)
+    who_im_following = models.ManyToManyField("self", related_name="my_followers", symmetrical=False, blank=True)
+    ignored = models.ManyToManyField("self", related_name="ignored_by", symmetrical=False, blank=True)
 
     def __str__(self):
         return self.user.get_username()
 
     def get_pending_friend_requests(self):
-        pending = self.followers.all()
-        pending = pending.exclude(pk__in=self.ignore.all()) # Ignore requests we have declined
-        pending = pending.exclude(pk__in=self.friends.all()) # Ignore people we are already friends with
+        """ Returns my pending friend requests
+        (people who have followed me, but I have not followed them) """
+        pending = self.my_followers.all()
+        pending = pending.exclude(pk__in=self.ignored.all()) # ignored requests we have declined
+        pending = pending.exclude(pk__in=self.friends.all()) # ignored people we are already friends with
         return pending
 
     def accept_friend_request(self, requester):
         """
-        When a friend request is excepted, both authors will be considered
-        followers of each other and friends of each other.
+        When a friend request is accepted, both authors will be considered
+        followers AND friends of each other.
         """
-        if requester not in self.followers.all():
-            raise ValueError("Attempted to accept friend request when requester is no longer following!")
+        if requester not in self.my_followers.all():
+            # Stale state, should probably reload the page
+            raise ValueError("Attempted to accept friend request when requester is no longer following me!")
         if requester not in self.friends.all():
             self.friends.add(requester)
-        if self not in requester.followers.all():
-            requester.followers.add(self)
+        if requester not in self.who_im_following.all():
+            self.who_im_following.add(requester)
         self.save()
-        requester.save()
         return
 
     def decline_friend_request(self, requester):
         """
         When we decline a friend request we simply move the requester into
-        our ignore queue (all this means is it won't show up in our friend requests).
+        our ignored queue (all this means is it won't show up in our friend requests).
         """
-        self.ignore.add(requester)
+        self.ignored.add(requester)
         self.save()
         return
 
     def delete_friend(self, friend):
         """ When we remove a friend, we unfriend and unfollow them. """
         self.friends.remove(friend)
-        friend.followers.remove(self)
+        self.who_im_following.remove(friend)
         self.save()
-        friend.save()
         return
 
     def follow(self, friend):
-        friend.followers.add(self)
+        self.who_im_following.add(friend)
         friend.save()
 
     def unfollow(self, friend):
-        friend.followers.remove(self)
+        self.who_im_following.remove(friend)
         friend.save()
 
 class Post(models.Model):
