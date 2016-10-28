b7a3d0388f266fe542ab8d018199d599cc4cfaf7
Victoria Bobey <vbobey@ualberta.ca>
2016-10-28 16:44:51 -0600

    Removed ability to use admin pages for Author model. Page is now for debugging.

--- mysite/socknet/admin.py
+++ mysite/socknet/admin.py
@@ -5,7 +5,7 @@ from socknet.models import *
 
 class UserAdmin(admin.ModelAdmin):
     """
-    Custom administrative page for the User model.
+    Custom administrative page and actions for the User model.
     """
     list_display = ['username', 'is_approved', 'date_joined']
     ordering = ['username']
@@ -48,7 +48,27 @@ class UserAdmin(admin.ModelAdmin):
             self.message_user(request, "%s successfully approved." % message_bit)
     approve_users.short_description = "Approve Selected Users"
 
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    A custom admin page for the Author model.
+    CURRENTLY CAN ONLY VIEW AUTHOR INFORMATION FOR TESTING PURPOSES
+    """
+    list_display = ['user']
+    ordering = ['user']
+    change_form_template = 'admin/view_author.html'
 
+    # Remove the delete Admin Action for this Model
+    actions = None
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def save_model(self, request, obj, form, change):
+        #Return nothing to make sure user can't update any data
+        pass
 
 # Unregister default user so that ours is used
 admin.site.unregister(User)
@@ -56,6 +76,6 @@ admin.site.register(User, UserAdmin)
 
 # Models we want to be able to edit in admin
 admin.site.register(Post)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Comment)
 admin.site.register(ImageServ)
