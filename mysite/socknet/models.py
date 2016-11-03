60be4338c9b125132227f7dd48053d1ba6e26bff
Victoria Bobey <vbobey@ualberta.ca>
2016-11-02 18:19:19 -0600

    UI clean up for profile and edit profile

--- mysite/socknet/models.py
+++ mysite/socknet/models.py
@@ -26,13 +26,12 @@ class Author(models.Model):
     friends = models.ManyToManyField("self", related_name="my_friends", blank=True)
     who_im_following = models.ManyToManyField("self", related_name="my_followers", symmetrical=False, blank=True)
     ignored = models.ManyToManyField("self", related_name="ignored_by", symmetrical=False, blank=True)
+
+    # Profile fields
     github_url = models.TextField(blank=True)
     about_me = models.CharField(max_length=1000, blank=True)
     birthday = models.DateField(null=True,blank=True)
 
-
-
-   
     def __str__(self):
         return self.user.get_username()
 
