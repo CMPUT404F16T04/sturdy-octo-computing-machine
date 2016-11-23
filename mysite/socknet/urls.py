62ffc8062168563bb61961ed5cc4401dfacb0f43
Carson McLean <camclean@ualberta.ca>
2016-11-22 18:12:55 -0700

    Corrects url for getting a specific author’s posts (/api/author/{AUTHOR_ID}/posts)

--- mysite/socknet/urls.py
+++ mysite/socknet/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     url(r'^api/posts/$', api_views.PostsQuery.as_view(), name="api_posts"),
     url(r'^api/author/posts', api_views.AuthorPostsViewSet.as_view(), name="api_author_posts"),
     url(r'^api/author/(?P<auth_id>[0-9A-Fa-f-]+)/posts/$', api_views.AuthorViewAllTheirPosts.as_view(), name="api_authors_posts"),
-    url(r'^api/author/(?P<authorid>[0-9A-Fa-f-]+)/', api_views.ProfileView.as_view(), name="api_profile_view"),
+    url(r'^api/author/(?P<authorid>[0-9A-Fa-f-]+)/$', api_views.ProfileView.as_view(), name="api_profile_view"),
 
     url(r'^api/posts/(?P<post_id>[0-9A-Fa-f-]+)/comments$', api_views.CommentsViewSet.as_view(), name="api_comments"),
 
